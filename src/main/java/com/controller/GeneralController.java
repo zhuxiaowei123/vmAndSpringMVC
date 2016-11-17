e0e54a9fa39664b2f73aa498a7458dc38c83a130
zhuxiaowei <zhuxiaowei@zhuxiaoweideMacBook-Air.local>
2016-11-17 13:36:23 +0800

    create project

---
+++ src/main/java/com/controller/GeneralController.java
@@ -0,0 +1,33 @@
+package com.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+@Controller
+public class GeneralController {
+
+    @RequestMapping(value = "/index.html")
+    public void index_jsp(Model model) {
+        model.addAttribute("str0121", "Hellow world");
+        System.out.println("index1.jsp");
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/index.json")
+    public ModelAndView doJson(Model model) {
+        model.addAttribute("str0121", "Hellow world");
+        System.out.println("json");
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("result", "success");
+        return new ModelAndView(new MappingJackson2JsonView(), map);
+
+    }
+}
